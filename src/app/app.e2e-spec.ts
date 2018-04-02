65a92461bb7f18670b3d1bc383ddd25f7d2e5e9e
Hudson Burgess <hudsonburgess7@gmail.com>
2018-04-02 17:46:34 -0400

    first run of e2e test from config file

--- src/app/app.e2e-spec.ts
+++ src/app/app.e2e-spec.ts
@@ -1,20 +1,22 @@
-import { Browser, BrowserType } from 'orange-peel';
+import { Browser } from 'orange-peel';
 
 describe('Khaleesi demo', () => {
 
     let driver: Browser;
 
     beforeAll(() => {
-        driver = new Browser(BrowserType.CHROME);
+        driver = new Browser('mouse');
     });
 
     afterAll(() => {
-        driver.quit();
+        setTimeout(() => {
+            driver.quit();
+        }, 1000);
     });
 
 
     it('should get the title from the home page', done => {
-        driver.get('http://localhost:4200')
+        driver.get('/')
             .then(() => {
             expect(true).toBeTruthy();
             done();
